4c6efcdf33439a63c86fe080731c4afc64244a36
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-20 20:58:14 +0700

    Full React Tutorial #7 - Click Events link : https://www.youtube.com/watch?v=0XSDAup85SA&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=7 - Events are what we click, tap, press in keyboard etc - We can configure events by adding onClick, onPress attribute in element that we target - When we use event, we can call action or function that avaiable - When we call function in events, we can't call directly, because function will not fire again if we do events - So we can use anonymous function, that anonymous function can handle this problem - We can throw events detail with throw a variable const handleClick = (e) => { console.log(e) } so we can do special action with event detail

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+   const handleClick = (e) => {
+      console.log('hello, ninjas!', e)
+   }
+
+   const handleClickAgain = (name, e) => {
+      console.log('hello ' + name, e.target);
+   }
+   
    return (
       <div className="home">
          <h2>Home page</h2>
+         <button onClick={handleClick}>Click Me</button>
+         {/* For not invoke function direcly, so we can use anonymouse functino */}
+         <button onClick={(e) => {
+            handleClickAgain('mario', e)
+         }}>Click me again</button>
       </div>
    )
 }
