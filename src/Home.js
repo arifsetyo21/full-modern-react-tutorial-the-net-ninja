87e30644d71fe49860cbe28b69388bb83a01c1dc
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-20 21:16:51 +0700

    Full React Tutorial #8 - Using State (useState Hook) link : https://www.youtube.com/watch?v=4pO-HcG2igk&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=8 - useState hook is a hook that allows you to have state variables that we can make components contain variable reactive - When can use useState with import that from react library - We can use `useState` eg. like this `const [name, setName] = useState('mario')` - That script with desctruct return of `useState('mario')` than the result is variable `name` that variable we can use for display with value of `mario`, and `setName` is function that can we use for change value of name reactively

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
-const Home = () => {
-   const handleClick = (e) => {
-      console.log('hello, ninjas!', e)
-   }
+import { useState } from "react";
 
-   const handleClickAgain = (name, e) => {
-      console.log('hello ' + name, e.target);
+const Home = () => {
+   // let name = 'mario'
+   const [name, setName] = useState('mario')
+   const [age, setAge] = useState(25)
+
+   const handleClick = () => {
+      setName('luigi')
+      setAge(30)
    }
    
    return (
       <div className="home">
          <h2>Home page</h2>
+         <p>{name} is { age } years old</p>
          <button onClick={handleClick}>Click Me</button>
-         {/* For not invoke function direcly, so we can use anonymouse functino */}
-         <button onClick={(e) => {
-            handleClickAgain('mario', e)
-         }}>Click me again</button>
       </div>
    )
 }
