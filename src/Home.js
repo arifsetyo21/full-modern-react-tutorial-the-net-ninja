06b9ba8e3a551091c59e61cb240271d373d90047
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 03:21:08 +0700

    Full React Tutorial #19 - Handling Fetch Errors link : https://www.youtube.com/watch?v=DTBta08fXGU&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=19 - Not forever our request is successful or ok - To handle unsuccessful request or error, we can use throw catch method - Firstly, we can check is response code succesful or fail, if response is fail we can throw error message and catch message with `catch()` - We can combine this handling error message with hooks, so the app can run informatively

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
    const [blogs, setBlogs] = useState(null)
    const [isPending, setIsPending] = useState(true)
+   const [error, setError] = useState(null)
 
    const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -13,16 +14,25 @@ const Home = () => {
    useEffect(() => {
       fetch('http://localhost:8000/blogs')
          .then(res => {
+            if (!res.ok) {
+               throw Error('could not fetch the data for that resource')
+            }
             return res.json()
          })
          .then(data => {
             setBlogs(data)
             setIsPending(false)
+            setError(null)
+         })
+         .catch(err => {
+            setIsPending(false)
+            setError(err.message)
          })
    }, [])
 
    return (
       <div className="home">
+         {error && <div>{ error }</div>}
          { isPending && <div>Loading...</div>}
          { blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} /> }
       </div>
