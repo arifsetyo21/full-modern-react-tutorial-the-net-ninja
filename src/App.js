10ed1f664ecf2ca93214ff83b76996b4a2b6f6bf
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-20 18:20:39 +0700

    Full React Tutorial #5 - Multiple Components link : https://www.youtube.com/watch?v=0sSYmRImgRY&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=5 - Create another component - Nesting component to root component - Using self-close tag element for simple write

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   const title = 'Welcome to the new blog'
@@ -8,16 +10,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        
-        <p>{10}</p>
-        <p>{"hello, ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-        
-        <a href={link}>Google Site</a>
+        <Home/>
       </div>
     </div>
   );
