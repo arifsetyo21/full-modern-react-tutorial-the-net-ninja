4cbb838711a5ec091260f8adf581aadc829a84c6
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 02:42:14 +0700

    Full React Tutorial #17 - Fetchingdata with useEffect link : https://www.youtube.com/watch?v=qdCHEUaFhBk&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=17 - In this section, we will fetching json data from json-server - We fetching data with useEffect hook, so data will fetching in first component rendered - When we fetching data, data not exactly collected directly, but data will take time to fully transfer - However, in BlogList component we use .map method, that will looping array from object or array, when object is null some error will happen, so for fixing this we can use when data still null, we not call the component

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-   const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-   ])
-
-   const [name, setName] = useState('mario')
+   const [blogs, setBlogs] = useState(null)
 
    const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -16,15 +10,19 @@ const Home = () => {
    }
 
    useEffect(() => {
-      console.log('use effect ran');
-      console.log(name);
-   }, [name])
+      fetch('http://localhost:8000/blogs')
+         .then(res => {
+            return res.json()
+         })
+         .then(data => {
+            console.log(data);
+            setBlogs(data)
+         })
+   }, [])
 
    return (
       <div className="home">
-         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-         <button onClick={() => setName('luigi')}>change name</button>
-         <p>{ name }</p>
+         {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
       </div>
    )
 }
