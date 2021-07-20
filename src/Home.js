93d59be9b6b2f4429f1dc869247d7c23254c1006
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 03:04:48 +0700

    Full React Tutorial #18 - Conditional Loading Message link : https://www.youtube.com/watch?v=qtheqr0jgIQ&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=18 - When our app is deployed in internet, we can't guaranted is our app can smoothly accessed - Sometimes when user access our app, when internet access or server is any problem, user will take too long time to complete request to our app - So we can put information into our app that our app is still processing the request, eg. text loading... - To handle this case, we can use `useState()` and `useEffect()` and put conditional state in our element

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
    const [blogs, setBlogs] = useState(null)
+   const [isPending, setIsPending] = useState(true)
 
    const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -15,14 +16,15 @@ const Home = () => {
             return res.json()
          })
          .then(data => {
-            console.log(data);
             setBlogs(data)
+            setIsPending(false)
          })
    }, [])
 
    return (
       <div className="home">
-         {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+         { isPending && <div>Loading...</div>}
+         { blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} /> }
       </div>
    )
 }
