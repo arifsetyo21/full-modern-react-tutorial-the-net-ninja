3adc890bb05ac2f074f95dc4b33d39bb12215c2b
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 02:01:51 +0700

    Full React Tutorial #15 - useEffect Depedencies link : https://www.youtube.com/watch?v=jQc_bTFZ5_I&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=15o - useEffect Depedencies is which variable if rerender will trigger useEffect hook - When useEffect Depedencies is empty, this mean useEffect only trigger on first render page - eg. `useEffect(() => { }, [name])` this will trigger when only variable name change

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
       { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
    ])
 
+   const [name, setName] = useState('mario')
+
    const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
       setBlogs(newBlogs)
@@ -15,11 +17,14 @@ const Home = () => {
 
    useEffect(() => {
       console.log('use effect ran');
-   })
+      console.log(name);
+   }, [name])
 
    return (
       <div className="home">
-         <BlogList blogs={blogs} title="All Blogs!" handleDelete={ handleDelete }/>
+         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+         <button onClick={() => setName('luigi')}>change name</button>
+         <p>{ name }</p>
       </div>
    )
 }
