f3c66644241f0e17f43498820de548ccb0fb16dc
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 14:26:58 +0700

    Full React Tutorial #21 - The React Router link : https://www.youtube.com/watch?v=aZGzwEjZrXc&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=21 - Non-react website : mean server side render, that browser will request and server will response with whole html page - React website : will create website with SPA's concept, that concept will only use an index page, and when any change in the page, react will only change or inject to the element concerned not the whole page - So, react will faster your app - To using this feature, we must using `react-router-dom` to control the router app - First, we must surround App.js component with `<Router>` - Then, we can use `<Switch>` for as parent route, and inside `<Switch>` tag, we can put `<Route path="path">` for define route the app - At last, we can put page or component that we want route inside the `<Route>`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar'
 import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   const title = 'Welcome to the new blog'
@@ -8,12 +9,18 @@ function App() {
   const link = "https://www.google.com"
 
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 
 }
