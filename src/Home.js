e8541dbbacb6ce72cba20a5ca091c90fa46f7526
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 13:51:32 +0700

    Full React Tutorial #20 - Making a custom hook link : https://www.youtube.com/watch?v=Jl4q2cccwf0&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=20 - When we will start create feature again, we may need again feature that we have created before, eg. fetch blog post in home component - So, we can create again the feature that we write before - This way is not efficient for time and code efficiency - So, we can write again the feature like as we create components, but this creature we can call custom hook - We can use alias in destruct variable  `const { data: blogs, isPending, error }`

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,15 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-   const [blogs, setBlogs] = useState(null)
-   const [isPending, setIsPending] = useState(true)
-   const [error, setError] = useState(null)
 
-   const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id)
-      setBlogs(newBlogs)
-   }
-
-   useEffect(() => {
-      fetch('http://localhost:8000/blogs')
-         .then(res => {
-            if (!res.ok) {
-               throw Error('could not fetch the data for that resource')
-            }
-            return res.json()
-         })
-         .then(data => {
-            setBlogs(data)
-            setIsPending(false)
-            setError(null)
-         })
-         .catch(err => {
-            setIsPending(false)
-            setError(err.message)
-         })
-   }, [])
+   const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
    return (
       <div className="home">
          {error && <div>{ error }</div>}
          { isPending && <div>Loading...</div>}
-         { blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} /> }
+         { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
       </div>
    )
 }
