b57ec69109134820dd1400c0ddca50183d38ce8d
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 21:28:38 +0700

    Full React Tutorial #24 - useEffect cleanup link : https://www.youtube.com/watch?v=aKOQtGLT-Yk&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=24 - When the data is still trying to fetch and then we go to another page before the fetch complete, we will get error - This happed because when the fetch is complete then we have to move to another page, then the home component isn't still in the page anymore, so the react can't update the component `Can't perform a React state update on an unmounted component.` - So, we need to stopping the fetch when the component unmounted, we will do it with combination of `cleanup` function and something called `abort controller`

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,8 +7,11 @@ const useFetch = (url) => {
    const [error, setError] = useState(null)
 
    useEffect(() => {
+
+      const abortCont = new AbortController()
+
       setTimeout(() => {
-         fetch(url)
+         fetch(url, { signal: abortCont.signal })
          .then(res => {
             if (!res.ok) {
                throw Error('could not fetch the data for that resource')
@@ -20,11 +23,18 @@ const useFetch = (url) => {
             setIsPending(false)
             setError(null)
          })
-         .catch(err => {
-            setIsPending(false)
-            setError(err.message)
+            .catch(err => {
+            if (err.name === "AbortError") {
+               console.log('fetch aborted');
+            } else {
+               setIsPending(false)
+               setError(err.message)
+            }
          })
       }, 1000);
+
+      return () => abortCont.abort();
+      
    }, [url])
 
    return {data, isPending, error}
