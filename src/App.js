b35475e3d8ea073228be27e4680c1bd78ebcc5ac
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-21 20:18:32 +0700

    Full React Tutorial #22 - Exact Match Routes link : https://www.youtube.com/watch?v=EmUa_tcSM-k&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=22 - When we create routes, the routes will matching url that we request to route, if route found and match with request url, react-router-dom will throw the match page that match first - Error come when we put root route in first, that will always return root page first - Then we can use `exact` attributes that will return only exact match routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Navbar from './Navbar'
 import Home from './Home'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from './Create';
 
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50
-  // const person = { name: 'yoshi', age: 30}
-  const link = "https://www.google.com"
-
   return (
     <Router>
       <div className="App">
         <Navbar/>
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home/>
+            </Route>
+            <Route path="/create">
+              <Create/>
             </Route>
           </Switch>
         </div>
