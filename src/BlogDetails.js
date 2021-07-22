9acaec88f68e264662bec06995c3a2cfe6812448
arifsetyo21 <arifsetyo19@gmail.com>
2021-07-22 15:45:23 +0700

    Full React Tutorial #31 - Deleting Blogs link : https://www.youtube.com/watch?v=XW0t2lk4Ffo&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=32 - - To avoid from leaking storage and clean our web app from unused blog post, we need a delete blog post feature

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,20 @@
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 import useFetch from './useFetch'
 
 const BlogDetails = () => {
    
    const { id } = useParams()
    const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`)
+   const history = useHistory()
    
+   const handleClick = () => {
+      fetch('http://localhost:8000/blogs/' + blog.id, {
+         method: 'DELETE'
+      }).then(() => {
+         history.push('/')
+      })
+   }
+
    return (
       <div className="blog-details">
          {isPending && <div>Loading...</div>}
@@ -17,6 +26,7 @@ const BlogDetails = () => {
                <div>{ blog.body }</div>
             </article>
          )}
+         <button onClick={handleClick}>Delete</button>
       </div>
    )
 }
